4cbb90f3bc3deb4498aedac4f8b30e76c62b7d4c
krongdew <dewwiisunny14@gmai.com>
2025-02-14 10:11:08 +0700

    navsidebar debug

--- src/app/[locale]/ClientWrapper.js
+++ src/app/[locale]/ClientWrapper.js
@@ -1,20 +1,28 @@
 "use client";
 
+import { DM_Sans } from "next/font/google";
 import { useEffect } from "react";
 import { usePathname } from "next/navigation";
-import toggleStore from "@/store/toggleStore";
-import SearchModal1 from "@/components/modal/SearchModal1";
 import BottomToTop from "@/components/button/BottomToTop";
+import SearchModal1 from "@/components/modal/SearchModal1";
 import NavSidebar from "@/components/sidebar/NavSidebar";
+import "react-tooltip/dist/react-tooltip.css";
 
+// Initialize Bootstrap on client side
 if (typeof window !== "undefined") {
   import("bootstrap");
 }
 
-export default function ClientWrapper({ children }) {
-  const isListingActive = toggleStore((state) => state.isListingActive);
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--font-dm-sans",
+});
+
+export default function ClientLayout({ children }) {
   const path = usePathname();
 
+  // Initialize WOW.js
   useEffect(() => {
     const { WOW } = require("wowjs");
     const wow = new WOW({
@@ -24,11 +32,15 @@ export default function ClientWrapper({ children }) {
   }, [path]);
 
   return (
-    <>
+    <div className={dmSans.className}>
       <SearchModal1 />
       {children}
+
+      {/* bottom to top */}
       <BottomToTop />
+
+      {/* sidebar mobile navigation */}
       <NavSidebar />
-    </>
+    </div>
   );
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,19 @@
-
 import './globals.css'
+import ClientLayout from './[locale]/ClientWrapper'
 
 export const metadata = {
   title: 'Prepmee',
   description: 'Prepmee',
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children, params }) {
   return (
-    <html>
-      <body>{children}</body>
+    <html lang={params.locale}>
+      <body>
+        <ClientLayout>
+          {children}
+        </ClientLayout>
+      </body>
     </html>
   )
 }

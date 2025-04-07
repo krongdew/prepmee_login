a73ebaa94b817e8135cbba0cd2689a828ca8d433
DewDewtopTop <dewtop@DewTops-Mac-mini.local>
2025-04-07 17:58:42 +0700

    output

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const withNextIntl = require('next-intl/plugin')('./src/i18n.js');
 
 const nextConfig = {
-    output: 'export',
     images: {
       unoptimized: true,
       formats: ['image/webp'],
